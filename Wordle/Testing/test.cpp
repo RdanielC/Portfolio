eafd37657d6e03b6eb129e8b73f673d58cbf1448
RdanielC <rolandocanales000@gmail.com>
2023-02-23 15:46:41 -0800

    nothing

--- Wordle/Testing/test.cpp
+++ Wordle/Testing/test.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
- 
+
 int main()
 {
-    std::string s = "C++,Java";
-    char ch = '+';
- 
-    int count = std::count(s.begin(), s.end(), ch);
-    std::cout << "Character " << ch << " occurs " << count << " times\n";
- 
-    return 0;
+    bool done = false;
+    std::string key = "robot";
+    std::string input;
+    std::string score = "-----";
+    //do {
+    std::cout << "enter guess: ";
+    std::cin >> input;
+
+    std::cout << "key: " << key << std::endl;
+    std::cout << "guess: " << input << std::endl;
+    std::cout << "score: " << score << std::endl;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            // r o b o t
+            // m o b o o
+            // - + - - -
+            if (key[i] == input[j] && j == i) {
+                score[i] = '+';
+            }
+        }
+        //done = true;
+    }
+    //} while (done == false);
+    std::cout << score << std::endl;
 }
